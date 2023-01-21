5b1fbca8e52cae5404562c8b28cac78fa88c3bb6
Rahix <rahix@rahix.de>
2023-01-21 01:52:56 +0100

    tests: Update PHY test
    Rewrite this test for the new PHY module design.

--- tests/phy.rs
+++ tests/phy.rs
@@ -8,27 +8,27 @@ fn dummy_phy_test() {
 
     let mut phy = phy::LinuxRs485Phy::new("/dev/ttyUSB0");
 
-    let mut buffer: phy::BufferHandle = vec![0u8; 256].into();
-    buffer[..6].copy_from_slice(&[0x10, 0x22, 0x02, 0x49, 0x6D, 0x16]);
+    phy.transmit_data(|buffer| {
+        buffer[..6].copy_from_slice(&[0x10, 0x22, 0x02, 0x49, 0x6D, 0x16]);
+        (6, ())
+    });
 
-    phy.schedule_tx(buffer, 6);
-    let buffer = loop {
-        if let Some(buf) = phy.poll_tx() {
-            break buf;
-        }
-        // don't busy spin
+    while phy.is_transmitting() {
         phy.wait_transmit();
-    };
+    }
     log::trace!("Sent request!");
 
-    phy.schedule_rx(buffer);
-
-    while phy.peek_rx().len() < 5 {
+    while phy.receive_data(|buffer| (0, buffer.len() < 5)) {
         std::thread::sleep(std::time::Duration::from_millis(1));
     }
 
-    let (buffer, length) = phy.poll_rx();
-    let msg = &buffer[..length];
+    let mut msg_buffer = [0u8; 256];
+    let msg = phy.receive_data(|buffer| {
+        assert!(buffer.len() >= 5);
+        let msg = &mut msg_buffer[..buffer.len()];
+        msg.copy_from_slice(buffer);
+        (buffer.len(), msg)
+    });
     log::debug!("{:?}", msg);
 
     let expected = [0x10, 0x02, 0x22, 0x00, 0x24, 0x16];

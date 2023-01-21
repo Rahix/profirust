c2c809c3b5f0b67d12f4065c81c02e776eb07e09
Rahix <rahix@rahix.de>
2023-01-21 02:24:29 +0100

    tests: Add FDL test

---
+++ tests/fdl.rs
@@ -0,0 +1,27 @@
+use profirust::fdl;
+use profirust::phy;
+
+#[ignore = "not connected"]
+#[test]
+fn dummy_fdl_test() {
+    env_logger::init();
+
+    let mut master = fdl::FdlMaster::new(fdl::Parameters {
+        address: 0x02,
+        baudrate: fdl::Baudrate::B31250,
+        ..Default::default()
+    });
+
+    log::debug!("{:#?}", master.parameters());
+    log::debug!(
+        "Lost token timeout: {:?} ms",
+        master.parameters().token_lost_timeout().millis()
+    );
+
+    let mut phy = phy::LinuxRs485Phy::new("/dev/ttyUSB0");
+
+    loop {
+        master.poll(profirust::time::Instant::now(), &mut phy);
+        std::thread::sleep(std::time::Duration::from_millis(10));
+    }
+}

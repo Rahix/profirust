c8e10bcd75cc6f041025db904026c0ff1c3fb828
Rahix <rahix@rahix.de>
2023-07-16 02:15:34 +0200

    peripheral: Stub until the peripheral talks!
    This is enough to get my remote I/O station to talk.  Yay!

--- src/peripheral/mod.rs
+++ src/peripheral/mod.rs
@@ -71,6 +71,9 @@ pub struct Peripheral<'a> {
     pi_i: &'a mut [u8],
     /// Process Image of Outputs
     pi_q: &'a mut [u8],
+    /// Last diagnostics request
+    last_diag: Option<crate::time::Instant>,
+    sent_diag: bool,
 }
 
 impl<'a> Peripheral<'a> {
@@ -151,7 +154,7 @@ impl<'a> Peripheral<'a> {
                     },
                     7,
                     |buf| {
-                        buf[0] = 0x00;
+                        buf[0] = 0x80;
                         // WD disabled
                         buf[1] = 0x00;
                         buf[2] = 0x00;
@@ -176,14 +179,35 @@ impl<'a> Peripheral<'a> {
                     },
                     2,
                     |buf| {
-                        buf[0] = 0x13; // 3 byte inputs
-                        buf[1] = 0x23; // 3 byte outputs
+                        buf[0] = 0x12; // 3 byte inputs
+                        buf[1] = 0x22; // 3 byte outputs
                     },
                 ))
             }
             PeripheralState::DataExchange => {
                 // Request diagnostics again
-                Some(self.send_diagnostics_request(master, tx))
+                let last_diag = self.last_diag.get_or_insert(now);
+                if (now - *last_diag) > crate::time::Duration::from_secs(1) {
+                    *last_diag = now;
+                    self.sent_diag = true;
+                    Some(self.send_diagnostics_request(master, tx))
+                } else {
+                    Some(tx.send_data_telegram(
+                        crate::fdl::DataTelegramHeader {
+                            da: self.address,
+                            sa: master.parameters().address,
+                            dsap: crate::consts::SAP_SLAVE_DATA_EXCHANGE,
+                            ssap: crate::consts::SAP_MASTER_DATA_EXCHANGE,
+                            fc: self.fcb.make_request_fc(crate::fdl::RequestType::SrdLow),
+                        },
+                        3,
+                        |buf| {
+                            buf[0] = 0x55;
+                            buf[1] = 0x55;
+                            buf[2] = 0x55;
+                        },
+                    ))
+                }
             }
             PeripheralState::Offline => unreachable!(),
         }
@@ -199,66 +223,8 @@ impl<'a> Peripheral<'a> {
             PeripheralState::Offline => unreachable!(),
             PeripheralState::Reset => {
                 // Diagnostics response
-                if let crate::fdl::Telegram::Data(t) = telegram {
-                    if t.h.dsap != Some(62) {
-                        log::warn!("Diagnostics response to wrong SAP: {t:?}");
-                        return;
-                    }
-                    if t.h.ssap != Some(60) {
-                        log::warn!("Diagnostics response from wrong SAP: {t:?}");
-                        return;
-                    }
-                    if t.pdu.len() < 6 {
-                        log::warn!("Diagnostics response too short: {t:?}");
-                        return;
-                    }
-
-                    let p_addr = self.address;
-                    let real_master = master.parameters().address;
-
-                    let station_not_ready = (t.pdu[0] & (1 << 1)) != 0;
-                    let config_fault = (t.pdu[0] & (1 << 2)) != 0;
-                    let ext_diag = (t.pdu[0] & (1 << 3)) != 0;
-                    let function_not_supported = (t.pdu[0] & (1 << 4)) != 0;
-                    let param_fault = (t.pdu[0] & (1 << 6)) != 0;
-
-                    let param_req = (t.pdu[1] & (1 << 0)) != 0;
-                    let stat_diag = (t.pdu[1] & (1 << 1)) != 0;
-                    let perm_on = (t.pdu[1] & (1 << 2)) != 0;
-                    if !perm_on {
-                        log::warn!("Inconsistent diagnostics!");
-                    }
-                    let watchdog_on = (t.pdu[1] & (1 << 3)) != 0;
-                    let freeze_mode = (t.pdu[1] & (1 << 4)) != 0;
-                    let sync_mode = (t.pdu[1] & (1 << 5)) != 0;
-
-                    let master_address = t.pdu[3];
-                    let ident_high = t.pdu[4];
-                    let ident_low = t.pdu[5];
-
-                    log::info!(
-                        r#"Peripheral Diagnostics (#{p_addr}):
- - Station Not Ready:       {station_not_ready}
- - Config Fault:            {config_fault}
- - Extended Diagnostics:    {ext_diag}
- - Function Not Supp.:      {function_not_supported}
- - Parameter Fault:         {param_fault}
- - Parameters Required:     {param_req}
- - Station Diagnostics:     {stat_diag}
- - Watchdog On:             {watchdog_on}
- - FREEZE Mode:             {freeze_mode}
- - SYNC Mode:               {sync_mode}
- - Master Address:          {master_address} (we are {real_master})
- - Ident:                   {ident_high:02x} {ident_low:02x}
- "#
-                    );
-
-                    self.fcb.cycle();
-
-                    // TODO: When not to do this?
+                if self.handle_diagnostics_response(master, &telegram) {
                     self.state = PeripheralState::WaitForParam;
-                } else {
-                    todo!()
                 }
             }
             PeripheralState::WaitForParam => {
@@ -279,7 +245,14 @@ impl<'a> Peripheral<'a> {
                     todo!()
                 }
             }
-            PeripheralState::DataExchange => todo!(),
+            PeripheralState::DataExchange => {
+                if self.sent_diag {
+                    self.sent_diag = false;
+                    self.handle_diagnostics_response(master, &telegram);
+                } else {
+                    log::debug!("{telegram:?}");
+                }
+            }
         }
     }
 
@@ -292,12 +265,78 @@ impl<'a> Peripheral<'a> {
             crate::fdl::DataTelegramHeader {
                 da: self.address,
                 sa: master.parameters().address,
-                dsap: Some(60),
-                ssap: Some(62),
+                dsap: crate::consts::SAP_SLAVE_DIAGNOSIS,
+                ssap: crate::consts::SAP_MASTER_MS0,
                 fc: self.fcb.make_request_fc(crate::fdl::RequestType::SrdLow),
             },
             0,
             |_buf| (),
         )
+    }
+
+    pub fn handle_diagnostics_response(
+        &mut self,
+        master: &crate::fdl::FdlMaster,
+        telegram: &crate::fdl::Telegram,
+    ) -> bool {
+        if let crate::fdl::Telegram::Data(t) = telegram {
+            if t.h.dsap != crate::consts::SAP_MASTER_MS0 {
+                log::warn!("Diagnostics response to wrong SAP: {t:?}");
+                return false;
+            }
+            if t.h.ssap != crate::consts::SAP_SLAVE_DIAGNOSIS {
+                log::warn!("Diagnostics response from wrong SAP: {t:?}");
+                return false;
+            }
+            if t.pdu.len() < 6 {
+                log::warn!("Diagnostics response too short: {t:?}");
+                return false;
+            }
+
+            let p_addr = self.address;
+            let real_master = master.parameters().address;
+
+            let station_not_ready = (t.pdu[0] & (1 << 1)) != 0;
+            let config_fault = (t.pdu[0] & (1 << 2)) != 0;
+            let ext_diag = (t.pdu[0] & (1 << 3)) != 0;
+            let function_not_supported = (t.pdu[0] & (1 << 4)) != 0;
+            let param_fault = (t.pdu[0] & (1 << 6)) != 0;
+
+            let param_req = (t.pdu[1] & (1 << 0)) != 0;
+            let stat_diag = (t.pdu[1] & (1 << 1)) != 0;
+            let perm_on = (t.pdu[1] & (1 << 2)) != 0;
+            if !perm_on {
+                log::warn!("Inconsistent diagnostics!");
+            }
+            let watchdog_on = (t.pdu[1] & (1 << 3)) != 0;
+            let freeze_mode = (t.pdu[1] & (1 << 4)) != 0;
+            let sync_mode = (t.pdu[1] & (1 << 5)) != 0;
+
+            let master_address = t.pdu[3];
+            let ident_high = t.pdu[4];
+            let ident_low = t.pdu[5];
+
+            log::info!(
+                r#"Peripheral Diagnostics (#{p_addr}):
+ - Station Not Ready:       {station_not_ready}
+ - Config Fault:            {config_fault}
+ - Extended Diagnostics:    {ext_diag}
+ - Function Not Supp.:      {function_not_supported}
+ - Parameter Fault:         {param_fault}
+ - Parameters Required:     {param_req}
+ - Station Diagnostics:     {stat_diag}
+ - Watchdog On:             {watchdog_on}
+ - FREEZE Mode:             {freeze_mode}
+ - SYNC Mode:               {sync_mode}
+ - Master Address:          {master_address} (we are {real_master})
+ - Ident:                   {ident_high:02x} {ident_low:02x}
+ "#
+            );
+
+            self.fcb.cycle();
+            true
+        } else {
+            todo!()
+        }
     }
 }

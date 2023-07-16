8b369f082f23aeedfcfd2d3b3c4d9841a81bcf2f
Rahix <rahix@rahix.de>
2023-07-16 02:14:52 +0200

    consts: Add most important SAP constants

--- src/consts.rs
+++ src/consts.rs
@@ -12,3 +12,25 @@ pub const SD4: u8 = 0xDC;
 pub const ED: u8 = 0x16;
 /// Short Confirmation
 pub const SC: u8 = 0xE5;
+
+/// SAP (Service Access Point) of an FDL master for **Data Exchange**
+pub const SAP_MASTER_DATA_EXCHANGE: Option<u8> = None;
+/// SAP (Service Access Point) of an FDL master for **DP MS2: Acyclic master class 2**
+pub const SAP_MASTER_MS2: Option<u8> = Some(50);
+/// SAP (Service Access Point) of an FDL master for **DP MS2: Acyclic master class 1**
+pub const SAP_MASTER_MS1: Option<u8> = Some(51);
+/// SAP (Service Access Point) of an FDL master for **DP master to master**
+pub const SAP_MASTER_MM: Option<u8> = Some(54);
+/// SAP (Service Access Point) of an FDL master for **DP MS0: slave handler per DP slave**
+pub const SAP_MASTER_MS0: Option<u8> = Some(62);
+
+/// SAP (Service Access Point) of a slave for **Data Exchange**
+pub const SAP_SLAVE_DATA_EXCHANGE: Option<u8> = None;
+/// SAP (Service Access Point) of a slave for **Get Configuration**
+pub const SAP_SLAVE_GET_CFG: Option<u8> = Some(59);
+/// SAP (Service Access Point) of a slave for **Slave Diagnosis**
+pub const SAP_SLAVE_DIAGNOSIS: Option<u8> = Some(60);
+/// SAP (Service Access Point) of a slave for **Set Parameters**
+pub const SAP_SLAVE_SET_PRM: Option<u8> = Some(61);
+/// SAP (Service Access Point) of a slave for **Check Configuration**
+pub const SAP_SLAVE_CHK_CFG: Option<u8> = Some(62);

Rahix/profirust

commit 98b472b0627f7ffa0d6826b9d42b1e8b2835bd0e

phy: simulator: Log incorrect delay between telegrams
PROFIBUS requires a minimum time of delay between consecutive
transmissions.  A master initiating a message cycle needs to wait 33 bit
times and a peripheral must wait at least minimum Tsdr which is at least
11 bit times.

As the PHY doesn't really know what kind of telegram it is dealing with,
let's at least check that 11 bit times were waited before responding.

As the FDL master implementation is currently non-compliant, only logan
error instead of panicking and thus failing the test.

src/phy/simulator.rs
@@ -34,7 +34,6 @@ impl SimulatorBus {
         let last_telegram = self.telegrams.last().unwrap();
 
         let last_telegram_tx_duration = self.bus_time - last_telegram.timestamp;
-        // TODO: Verify byte == 11 bit times
         let last_telegram_tx_bytes =
             (self.baudrate.time_to_bits(last_telegram_tx_duration) / 11) as usize;
 
@@ -53,7 +52,6 @@ impl SimulatorBus {
         let last_telegram = self.telegrams.last().unwrap();
 
         let last_telegram_tx_duration = self.bus_time - last_telegram.timestamp;
-        // TODO: Verify byte == 11 bit times
         let last_telegram_tx_bytes =
             (self.baudrate.time_to_bits(last_telegram_tx_duration) / 11) as usize;
 
@@ -70,6 +68,20 @@ impl SimulatorBus {
                 "\"{}\" attempted transmission while \"{}\" is still sending!",
                 name, active_sender
             );
+        }
+
+        // Ensure that at least 11 bit times were left between two consecutive transmissions.
+        if let Some(last_telegram_and_pause) = self
+            .telegrams
+            .last()
+            .map(|t| t.timestamp + self.baudrate.bits_to_time(t.length as u32 * 11 + 11))
+        {
+            if self.bus_time < last_telegram_and_pause {
+                log::error!(
+                    "\"{}\" did not leave minimum Tsdr time before its transmission.",
+                    name
+                );
+            }
         }
 
         if let Some(Ok(decoded)) = crate::fdl::Telegram::deserialize(&data) {

commit bcbed86f6168d802959e84b3ce8206e502cb31a2

phy: simulator: Enforce delay between telegrams
The FDL master implementation now passes this requirement in all places
reached by the testsuite so we can now enforce the delay properly.

Fixes: 98b472b0627f ("phy: simulator: Log incorrect delay between telegrams")

src/phy/simulator.rs
@@ -107,7 +107,7 @@ impl SimulatorBus {
                         name
                     );
                 } else {
-                    log::error!(
+                    panic!(
                         "\"{}\" did not leave minimum Tsdr time before its transmission.",
                         name
                     );
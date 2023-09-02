46a29a7b75effbf6958c6f84453977fd17bdbb0c
Rahix <rahix@rahix.de>
2023-09-02 14:46:10 +0200

    fdl: Re-export missing DataTelegram type

--- src/fdl/mod.rs
+++ src/fdl/mod.rs
@@ -8,6 +8,6 @@ pub use master::{FdlMaster, OperatingState};
 pub use parameters::Parameters;
 pub use peripheral_set::{PeripheralHandle, PeripheralSet, PeripheralStorage};
 pub use telegram::{
-    DataTelegramHeader, FrameCountBit, FunctionCode, RequestType, ResponseState, ResponseStatus,
-    ShortConfirmation, Telegram, TelegramTx, TelegramTxResponse, TokenTelegram,
+    DataTelegram, DataTelegramHeader, FrameCountBit, FunctionCode, RequestType, ResponseState,
+    ResponseStatus, ShortConfirmation, Telegram, TelegramTx, TelegramTxResponse, TokenTelegram,
 };

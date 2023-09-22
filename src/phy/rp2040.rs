86789573bc610e05046586cacfebf134818442a6
Rahix <rahix@rahix.de>
2023-09-23 01:32:18 +0200

    phy: rp2040: Continue working on the implementation

--- src/phy/rp2040.rs
+++ src/phy/rp2040.rs
@@ -1,21 +1,61 @@
+use embedded_hal::digital::v2::OutputPin;
 use rp2040_hal::uart;
 
 use fugit::RateExtU32;
 use rp2040_hal::Clock;
 
 #[derive(Debug)]
-pub struct Rp2040Phy<'a, UART> {
-    uart: UART,
-    buf: crate::phy::BufferHandle<'a>,
+enum PhyData<'a> {
+    Rx {
+        buffer: crate::phy::BufferHandle<'a>,
+        length: usize,
+    },
+    Tx {
+        buffer: crate::phy::BufferHandle<'a>,
+        length: usize,
+        cursor: usize,
+    },
 }
 
-impl<'a, D, P> Rp2040Phy<'a, uart::UartPeripheral<uart::Enabled, D, P>>
+impl PhyData<'_> {
+    pub fn is_rx(&self) -> bool {
+        match self {
+            PhyData::Rx { .. } => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_tx(&self) -> bool {
+        match self {
+            PhyData::Tx { .. } => true,
+            _ => false,
+        }
+    }
+
+    pub fn make_rx(&mut self) {
+        if let PhyData::Tx { buffer, .. } = self {
+            let buffer = core::mem::replace(buffer, (&mut [][..]).into());
+            *self = PhyData::Rx { buffer, length: 0 };
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Rp2040Phy<'a, UART, DIR> {
+    uart: UART,
+    dir_pin: DIR,
+    data: PhyData<'a>,
+}
+
+impl<'a, D, P, DIR> Rp2040Phy<'a, uart::UartPeripheral<uart::Enabled, D, P>, DIR>
 where
     D: uart::UartDevice,
     P: uart::ValidUartPinout<D>,
+    DIR: OutputPin,
 {
     pub fn new(
         uart: uart::UartPeripheral<uart::Disabled, D, P>,
+        mut dir_pin: DIR,
         per_clock: &rp2040_hal::clocks::PeripheralClock,
         buffer: impl Into<crate::phy::BufferHandle<'a>>,
         baudrate: crate::Baudrate,
@@ -30,33 +70,121 @@ where
             per_clock.freq(),
         )?;
 
+        // Go into RX mode.
+        dir_pin.set_low().ok().unwrap();
+
         Ok(Self {
             uart,
-            buf: buffer.into(),
+            dir_pin,
+            data: PhyData::Rx {
+                buffer: buffer.into(),
+                length: 0,
+            },
         })
     }
 }
 
-impl<'a, D, P> crate::phy::ProfibusPhy for Rp2040Phy<'a, uart::UartPeripheral<uart::Enabled, D, P>>
+impl<'a, D, P, DIR> crate::phy::ProfibusPhy
+    for Rp2040Phy<'a, uart::UartPeripheral<uart::Enabled, D, P>, DIR>
 where
     D: uart::UartDevice,
     P: uart::ValidUartPinout<D>,
+    DIR: OutputPin,
 {
     fn is_transmitting(&mut self) -> bool {
-        todo!()
+        if let PhyData::Tx {
+            buffer,
+            length,
+            cursor,
+        } = &mut self.data
+        {
+            if length != cursor {
+                let written = match self.uart.write_raw(&buffer[*cursor..*length]) {
+                    Ok(b) => b.len(),
+                    Err(nb::Error::WouldBlock) => 0,
+                    Err(nb::Error::Other(_)) => unreachable!(),
+                };
+                debug_assert!(written <= *length - *cursor);
+                *cursor += written;
+                true
+            } else {
+                // TODO: Check for queued bytes
+                self.data.make_rx();
+                self.dir_pin.set_low().ok().unwrap();
+                false
+            }
+        } else {
+            false
+        }
     }
 
     fn transmit_data<F, R>(&mut self, f: F) -> R
     where
         F: FnOnce(&mut [u8]) -> (usize, R),
     {
-        todo!()
+        match &mut self.data {
+            PhyData::Tx { .. } => panic!("transmit_data() while already transmitting!"),
+            PhyData::Rx {
+                buffer,
+                length: receive_length,
+            } => {
+                if *receive_length != 0 {
+                    log::warn!(
+                        "{} bytes in the receive buffer and we go into transmission?",
+                        receive_length
+                    );
+                }
+                let (length, res) = f(&mut buffer[..]);
+                if length == 0 {
+                    // Don't transmit anything.
+                    return res;
+                }
+
+                // TODO: timing considerations?  for now, let's only set the pin here and hope that
+                // enough time passes before the next `is_transmitting()` call happens.
+                self.dir_pin.set_high().ok().unwrap();
+
+                let cursor = 0; /*match self.uart.write_raw(&buffer[..length]) {
+                                    Ok(b) => b.len(),
+                                    Err(nb::Error::WouldBlock) => 0,
+                                    Err(nb::Error::Other(_)) => unreachable!(),
+                                };*/
+                debug_assert!(cursor <= length);
+                let buffer = core::mem::replace(buffer, (&mut [][..]).into());
+                self.data = PhyData::Tx {
+                    buffer,
+                    length,
+                    cursor,
+                };
+                res
+            }
+        }
     }
 
     fn receive_data<F, R>(&mut self, f: F) -> R
     where
         F: FnOnce(&[u8]) -> (usize, R),
     {
-        todo!()
+        match &mut self.data {
+            PhyData::Tx { .. } => panic!("receive_data() while transmitting!"),
+            PhyData::Rx { buffer, length } => {
+                *length += match self.uart.read_raw(&mut buffer[*length..]) {
+                    Ok(l) => l,
+                    Err(nb::Error::WouldBlock) => 0,
+                    Err(nb::Error::Other(_)) => {
+                        // TODO: handle uart errors
+                        0
+                    }
+                };
+                debug_assert!(*length <= buffer.len());
+                let (drop, res) = f(&buffer[..*length]);
+                match drop {
+                    0 => (),
+                    d if d == *length => *length = 0,
+                    d => todo!("drop partial receive buffer ({} bytes of {})", d, *length),
+                }
+                res
+            }
+        }
     }
 }

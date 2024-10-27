a596a75ec73d2fcc905ef3a05430cdca6516700e
Rahix <rahix@rahix.de>
2024-10-27 03:16:17 +0100

    fdl: token_ring: Implement LAS filling

--- src/fdl/token_ring.rs
+++ src/fdl/token_ring.rs
@@ -1,6 +1,8 @@
 /// Status of the `LAS` (List of Active Stations)
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum LasState {
+    /// We are waiting for the first token rotation to then enter discovery
+    Uninitialized,
     /// We are listening to the first token rotation to discover all active stations.
     Discovery,
     /// We are listening to the second token rotation to verify correctness of the LAS.
@@ -58,7 +60,7 @@ impl TokenRing {
 
         Self {
             active_stations,
-            las_state: LasState::Discovery,
+            las_state: LasState::Uninitialized,
             this_station: param.address,
             next_station: param.address,
             previous_station: param.address,
@@ -79,17 +81,251 @@ impl TokenRing {
         self.this_station
     }
 
-    pub fn next_station(&self) -> u8 {
+    pub fn next_station(&self) -> crate::Address {
         self.next_station
     }
 
-    pub fn previous_station(&self) -> u8 {
+    pub fn previous_station(&self) -> crate::Address {
         self.previous_station
+    }
+
+    fn verify_las_from_token_pass(&mut self, sa: crate::Address, da: crate::Address) -> bool {
+        // SA station must be active
+        if !self.active_stations[usize::from(sa)] {
+            return false;
+        }
+
+        // DA station must be active
+        if !self.active_stations[usize::from(da)] {
+            return false;
+        }
+
+        // No stations between SA and DA must be active
+        if da > sa {
+            if self.active_stations[usize::from(sa + 1)..usize::from(da)].any() {
+                return false;
+            }
+        } else {
+            // Handle wrap-around
+            if self.active_stations[usize::from(sa + 1)..].any() {
+                return false;
+            }
+            if self.active_stations[..usize::from(da)].any() {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn update_las_from_token_pass(&mut self, sa: crate::Address, da: crate::Address) {
+        // Clear the GAP from this token pass as it does not contain any known active stations.
+        if da > sa {
+            self.active_stations[usize::from(sa)..usize::from(da)].fill(false);
+        } else {
+            self.active_stations[usize::from(sa)..].fill(false);
+            self.active_stations[..usize::from(da)].fill(false);
+        }
+
+        // At this point, we only know that the source address is alive so we only enter it into
+        // the LAS.  The destination will be added later, when it actively forwards the token
+        // itself.
+        self.active_stations.set(usize::from(sa), true);
+    }
+
+    pub fn witness_token_pass(&mut self, sa: crate::Address, da: crate::Address) {
+        match self.las_state {
+            // If we see the wrap-around, start discovery
+            LasState::Uninitialized => {
+                if da <= sa {
+                    self.las_state = LasState::Discovery;
+                }
+            }
+            LasState::Discovery => {
+                self.update_las_from_token_pass(sa, da);
+                if da <= sa {
+                    self.las_state = LasState::Verification;
+                }
+            }
+            LasState::Verification => {
+                // If verification fails, restart discovery
+                if !self.verify_las_from_token_pass(sa, da) {
+                    self.update_las_from_token_pass(sa, da);
+                    self.las_state = LasState::Discovery;
+                } else if da <= sa {
+                    self.las_state = LasState::Valid;
+                }
+            }
+            LasState::Valid => {
+                self.update_las_from_token_pass(sa, da);
+            }
+        }
     }
 }
 
 impl core::fmt::Debug for TokenRing {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        todo!()
+        let mut active_stations = [0u8; 127];
+        let num_stations = self.iter_active_stations().count();
+        for (i, addr) in self.iter_active_stations().enumerate() {
+            active_stations[i] = addr;
+        }
+        f.debug_struct("TokenRing")
+            .field("previous_station", &self.previous_station)
+            .field("this_station", &self.this_station)
+            .field("next_station", &self.next_station)
+            .field("las_state", &self.las_state)
+            .field("active_stations", &&active_stations[..num_stations])
+            .finish()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use proptest::prelude::*;
+
+    #[test]
+    fn test_token_ring_add_some_stations() {
+        let mut token_ring = TokenRing::new(&Default::default());
+
+        token_ring.witness_token_pass(29, 3);
+        token_ring.witness_token_pass(3, 15);
+        token_ring.witness_token_pass(15, 29);
+
+        dbg!(&token_ring);
+    }
+
+    #[test]
+    fn test_las_initialization_bad_verification() {
+        let mut token_ring = TokenRing::new(&crate::fdl::Parameters {
+            address: 7,
+            ..Default::default()
+        });
+
+        assert_eq!(token_ring.las_state, LasState::Uninitialized);
+
+        token_ring.witness_token_pass(15, 29);
+        token_ring.witness_token_pass(29, 3);
+
+        assert_eq!(token_ring.las_state, LasState::Discovery);
+
+        token_ring.witness_token_pass(3, 15);
+        token_ring.witness_token_pass(15, 29);
+        token_ring.witness_token_pass(29, 3);
+
+        assert_eq!(token_ring.las_state, LasState::Verification);
+
+        token_ring.witness_token_pass(3, 15);
+        token_ring.witness_token_pass(15, 18);
+        token_ring.witness_token_pass(18, 29);
+
+        assert_eq!(token_ring.las_state, LasState::Discovery);
+
+        token_ring.witness_token_pass(29, 3);
+
+        assert_eq!(token_ring.las_state, LasState::Verification);
+
+        token_ring.witness_token_pass(3, 15);
+        token_ring.witness_token_pass(15, 29);
+
+        assert_eq!(token_ring.las_state, LasState::Discovery);
+
+        token_ring.witness_token_pass(29, 3);
+
+        assert_eq!(token_ring.las_state, LasState::Verification);
+
+        token_ring.witness_token_pass(3, 15);
+        token_ring.witness_token_pass(15, 29);
+        token_ring.witness_token_pass(29, 3);
+
+        assert_eq!(token_ring.las_state, LasState::Valid);
+        assert!(token_ring.ready_for_ring());
+    }
+
+    proptest! {
+        #[test]
+        fn test_las_update_correctness(
+            previous in prop::collection::vec(any::<bool>(), 126),
+            da in 0..126u8,
+            sa in 0..126u8,
+        ) {
+            let mut token_ring = TokenRing::new(&crate::fdl::Parameters {
+                address: 7,
+                ..Default::default()
+            });
+            for (mut station, state) in token_ring.active_stations.iter_mut().zip(previous.iter()) {
+                *station = *state;
+            }
+
+            let current = token_ring.active_stations.clone();
+            let verify = token_ring.verify_las_from_token_pass(sa, da);
+
+            token_ring.update_las_from_token_pass(sa, da);
+
+            if !verify {
+                // Also put the destination address into the LAS to make verification happy
+                token_ring.active_stations.set(usize::from(da), true);
+            }
+
+            if verify {
+                assert_eq!(token_ring.active_stations, current);
+            }
+
+            assert!(token_ring.verify_las_from_token_pass(sa, da));
+
+            let current = token_ring.active_stations.clone();
+            token_ring.update_las_from_token_pass(sa, da);
+            assert_eq!(token_ring.active_stations, current);
+        }
+
+        #[test]
+        fn test_las_initialization_happy_path(
+            mut active_stations in prop::collection::vec(0..126u8, 1..16),
+        ) {
+            let mut token_ring = TokenRing::new(&crate::fdl::Parameters {
+                address: 7,
+                ..Default::default()
+            });
+
+            active_stations.sort();
+            active_stations.dedup();
+
+            assert_eq!(token_ring.las_state, LasState::Uninitialized);
+
+            for addresses in active_stations.windows(2) {
+                let prev = addresses[0];
+                let next = addresses[1];
+                token_ring.witness_token_pass(prev, next);
+            }
+            // Wrap-around
+            token_ring.witness_token_pass(active_stations[active_stations.len() - 1], active_stations[0]);
+
+            assert_eq!(token_ring.las_state, LasState::Discovery);
+
+            for addresses in active_stations.windows(2) {
+                let prev = addresses[0];
+                let next = addresses[1];
+                token_ring.witness_token_pass(prev, next);
+            }
+            // Wrap-around
+            token_ring.witness_token_pass(active_stations[active_stations.len() - 1], active_stations[0]);
+
+            assert_eq!(token_ring.las_state, LasState::Verification);
+
+            for addresses in active_stations.windows(2) {
+                let prev = addresses[0];
+                let next = addresses[1];
+                token_ring.witness_token_pass(prev, next);
+            }
+            // Wrap-around
+            token_ring.witness_token_pass(active_stations[active_stations.len() - 1], active_stations[0]);
+
+            assert_eq!(token_ring.las_state, LasState::Valid);
+            assert!(token_ring.ready_for_ring());
+
+            let known_stations = token_ring.iter_active_stations().collect::<Vec<_>>();
+            assert_eq!(active_stations, known_stations);
+        }
     }
 }

Rahix/profirust

commit 62c224e84d08be7b6e3905b78e7efa6bbc958ffd

dp: peripheral: Store process images in `managed` slices
Use the `ManagedSlice` type to store the process image buffers.  This
allows owning the buffers on platforms where an allocator is available.
Owned buffers are significantly easier to use in dynamic environments.

src/dp/peripheral.rs
@@ -170,7 +170,7 @@ enum PeripheralState {
 ///     }
 /// }
 /// ```
-#[derive(Debug, PartialEq, Eq, Default)]
+#[derive(Debug)]
 pub struct Peripheral<'a> {
     /// Station address of this peripheral (slave)
     address: u8,
@@ -184,9 +184,9 @@ pub struct Peripheral<'a> {
     /// side.
     fcb: crate::fdl::FrameCountBit,
     /// Process Image of Inputs
-    pi_i: &'a mut [u8],
+    pi_i: managed::ManagedSlice<'a, u8>,
     /// Process Image of Outputs
-    pi_q: &'a mut [u8],
+    pi_q: managed::ManagedSlice<'a, u8>,
     /// Last diagnostics request
     diag: Option<DiagnosticsInfo>,
     /// Storage for extended diagnostics (if available)
@@ -197,20 +197,36 @@ pub struct Peripheral<'a> {
     options: PeripheralOptions<'a>,
 }
 
+impl Default for Peripheral<'_> {
+    fn default() -> Self {
+        Self {
+            address: Default::default(),
+            state: Default::default(),
+            retry_count: Default::default(),
+            fcb: Default::default(),
+            pi_i: [].into(),
+            pi_q: [].into(),
+            diag: Default::default(),
+            ext_diag: Default::default(),
+            diag_needed: Default::default(),
+            options: Default::default(),
+        }
+    }
+}
+
 impl<'a> Peripheral<'a> {
     /// Construct a new peripheral from its address, options, and buffers for the process image of
     /// inputs (`pi_i`) and process image of outputs (`pi_q`).
-    pub fn new(
-        address: u8,
-        options: PeripheralOptions<'a>,
-        pi_i: &'a mut [u8],
-        pi_q: &'a mut [u8],
-    ) -> Self {
+    pub fn new<PII, PIQ>(address: u8, options: PeripheralOptions<'a>, pi_i: PII, pi_q: PIQ) -> Self
+    where
+        PII: Into<managed::ManagedSlice<'a, u8>>,
+        PIQ: Into<managed::ManagedSlice<'a, u8>>,
+    {
         Self {
             address,
             options,
-            pi_i,
-            pi_q,
+            pi_i: pi_i.into(),
+            pi_q: pi_q.into(),
             ..Default::default()
         }
     }
@@ -235,8 +251,8 @@ impl<'a> Peripheral<'a> {
     /// place once the device responds at the new address.
     pub fn reset_address(&mut self, new_address: crate::Address) {
         let options = core::mem::take(&mut self.options);
-        let pi_i = core::mem::take(&mut self.pi_i);
-        let pi_q = core::mem::take(&mut self.pi_q);
+        let pi_i = core::mem::replace(&mut self.pi_i, [].into());
+        let pi_q = core::mem::replace(&mut self.pi_q, [].into());
         let diag_buffer = self.ext_diag.take_buffer();
 
         *self = Self::new(new_address, options, pi_i, pi_q).with_diag_buffer(diag_buffer);

commit 193ffea5b2fb863dfa1b5ce24c4384cd2f1fadd9

dp: Fix broken no_std builds
In commits 62c224e84d08 ("dp: peripheral: Store process images in
`managed` slices") and 838ed88791ee ("dp: diagnostics: Store diagnostics
in a `managed` slice"), `ManagedSlice`s were introduced for storing the
buffers of a peripheral.  Unfortunately, a small oversight where an
std-only conversion was used lead to these commits breaking `no_std`
usage of profirust.

Compiler errors like this one could be observed by downstream users
trying to build profirust without the `std` feature:

	error[E0277]: the trait bound `ManagedSlice<'_, u8>: From<[_; 0]>` is not satisfied

Fortunately, this is easily fixed by using a different approach for
creating empty `ManagedSlice`s.

Fixes: 62c224e84d08 ("dp: peripheral: Store process images in `managed` slices")
Fixes: 838ed88791ee ("dp: diagnostics: Store diagnostics in a `managed` slice")

src/dp/diagnostics.rs
@@ -10,7 +10,7 @@ pub struct ExtendedDiagnostics<'a> {
 impl Default for ExtendedDiagnostics<'_> {
     fn default() -> Self {
         Self {
-            buffer: [].into(),
+            buffer: managed::ManagedSlice::Borrowed(&mut []),
             length: 0,
         }
     }
@@ -56,7 +56,7 @@ impl<'a> ExtendedDiagnostics<'a> {
 
     pub(crate) fn take_buffer(&mut self) -> managed::ManagedSlice<'a, u8> {
         self.length = 0;
-        core::mem::replace(&mut self.buffer, [].into())
+        core::mem::replace(&mut self.buffer, managed::ManagedSlice::Borrowed(&mut []))
     }
 
     pub(crate) fn fill(&mut self, buf: &[u8]) -> bool {

src/dp/peripheral.rs
@@ -207,8 +207,8 @@ impl Default for Peripheral<'_> {
             state: Default::default(),
             retry_count: Default::default(),
             fcb: Default::default(),
-            pi_i: [].into(),
-            pi_q: [].into(),
+            pi_i: managed::ManagedSlice::Borrowed(&mut []),
+            pi_q: managed::ManagedSlice::Borrowed(&mut []),
             diag: Default::default(),
             ext_diag: Default::default(),
             diag_needed: Default::default(),
@@ -259,8 +259,8 @@ impl<'a> Peripheral<'a> {
     /// place once the device responds at the new address.
     pub fn reset_address(&mut self, new_address: crate::Address) {
         let options = core::mem::take(&mut self.options);
-        let pi_i = core::mem::replace(&mut self.pi_i, [].into());
-        let pi_q = core::mem::replace(&mut self.pi_q, [].into());
+        let pi_i = core::mem::replace(&mut self.pi_i, managed::ManagedSlice::Borrowed(&mut []));
+        let pi_q = core::mem::replace(&mut self.pi_q, managed::ManagedSlice::Borrowed(&mut []));
         let diag_buffer = self.ext_diag.take_buffer();
 
         *self = Self::new(new_address, options, pi_i, pi_q).with_diag_buffer(diag_buffer);
Rahix/profirust

commit 05150b92629541ece4f067c756c0d76e31cb474e

fdl: active: Ensure GAP never scans own address
Add a debug assertion to ensure the GAP scan never sends a status
request to the address of the active station itself.

src/fdl/active.rs
@@ -1128,6 +1128,8 @@ impl FdlActiveStation {
             }
 
             if let GapState::DoPoll { current_address } = self.gap_state {
+                debug_assert_ne!(current_address, self.p.address);
+
                 let tx_res = phy
                     .transmit_telegram(now, |tx| {
                         Some(tx.send_fdl_status_request(current_address, self.p.address))

commit 77c578171c9ceb8c4b331bfad1f1bc22e0657e51

fdl: active: Fix GAP polling when NS==TS
Ensure the active station does not poll itself when the next_station is
also the active station itself (= it is alone on the bus).

Fixes: 05150b926295 ("fdl: active: Ensure GAP never scans own address")

src/fdl/active.rs
@@ -666,6 +666,9 @@ impl FdlActiveStation {
         if next_address >= next_station && next_station > self.p.address {
             // We have reached the end of the GAP, enter waiting state.
             GapState::Waiting { rotation_count: 0 }
+        } else if next_address == next_station && next_station == self.p.address {
+            // We have reached the end of the GAP, enter waiting state (NS==TS case).
+            GapState::Waiting { rotation_count: 0 }
         } else if next_address >= next_station
             && next_station < self.p.address
             && next_address < self.p.address